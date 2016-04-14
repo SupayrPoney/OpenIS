e71201fd7fc7d7f08e6c3fd15f82494afd172e23
iTitou <moiandme@gmail.com>
2016-04-14 17:32:50 +0200

    [add] Workout and heart rate

--- database/create.sql
+++ database/create.sql
@@ -1,3 +1,5 @@
+CREATE TABLE "workout" ("id" SERIAL PRIMARY KEY);
+
 CREATE TABLE "activity_type" (
   "id" SERIAL PRIMARY KEY,
   "name" TEXT NOT NULL
@@ -52,17 +54,20 @@ CREATE TABLE "exercise_rating" (
 CREATE INDEX "idx_exercise_rating__exercise_spot" ON "exercise_rating" ("exercise_spot_id");
 CREATE INDEX "idx_exercise_rating__user" ON "exercise_rating" ("user_id");
 
+ALTER TABLE exercise_rating ADD CONSTRAINT grade_check CHECK (grade >= 0 AND grade <= 10);
 ALTER TABLE "exercise_rating" ADD CONSTRAINT "fk_exercise_rating__exercise_spot" FOREIGN KEY ("exercise_spot_id") REFERENCES "exercise_spot" ("id");
 ALTER TABLE "exercise_rating" ADD CONSTRAINT "fk_exercise_rating__user" FOREIGN KEY ("user_id") REFERENCES "user" ("id");
 
 CREATE TABLE "record" (
   "id" SERIAL PRIMARY KEY,
+  "workout_id" INTEGER NOT NULL,
   "start_time" TIMESTAMP NOT NULL,
   "end_time" TIMESTAMP NOT NULL,
   "calories" REAL NOT NULL,
   "track_id" INTEGER NOT NULL,
   "activity_type_id" INTEGER NOT NULL,
-  "user_id" INTEGER NOT NULL
+  "user_id" INTEGER NOT NULL,
+  "heart_rate" INTEGER NOT NULL
 );
 
 CREATE INDEX "idx_record__activitytype" ON "record" ("activity_type_id");
@@ -72,6 +77,7 @@ CREATE INDEX "idx_record__user" ON "record" ("user_id");
 ALTER TABLE "record" ADD CONSTRAINT "fk_record__activity_type" FOREIGN KEY ("activity_type_id") REFERENCES "activity_type" ("id");
 ALTER TABLE "record" ADD CONSTRAINT "fk_record__track" FOREIGN KEY ("track_id") REFERENCES "track" ("id");
 ALTER TABLE "record" ADD CONSTRAINT "fk_record__user" FOREIGN KEY ("user_id") REFERENCES "user" ("id");
+ALTER TABLE "record" ADD CONSTRAINT "fk_record__workout" FOREIGN KEY ("workout_id") REFERENCES "workout" ("id");
 
 CREATE TABLE "rating" (
   "record_id" INTEGER PRIMARY KEY,
@@ -79,4 +85,25 @@ CREATE TABLE "rating" (
   "review" TEXT NOT NULL
 );
 
+ALTER TABLE rating ADD CONSTRAINT grade_check CHECK (grade >= 0 AND grade <= 10);
 ALTER TABLE "rating" ADD CONSTRAINT "fk_rating__record" FOREIGN KEY ("record_id") REFERENCES "record" ("id");
+
+
+
+CREATE TABLE "exercise_session" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" INTEGER NOT NULL,
+  "workout_id" INTEGER NOT NULL,
+  "exercise_spot" INTEGER NOT NULL,
+  "repetitions" INTEGER NOT NULL,
+  "calories" REAL NOT NULL,
+  "difficulty" INTEGER NOT NULL,
+  "start_time" TIMESTAMP NOT NULL,
+  "end_time" TIMESTAMP NOT NULL,
+  "heart_rate" INTEGER NOT NULL
+);
+
+ALTER TABLE exercise_session ADD CONSTRAINT "fk_exercise_session__spot" FOREIGN KEY ("exercise_spot") REFERENCES "exercise_spot" ("id");
+ALTER TABLE exercise_session ADD CONSTRAINT "fk_exercise_session__user" FOREIGN KEY ("user_id") REFERENCES "user" ("id");
+ALTER TABLE exercise_session ADD CONSTRAINT "fk_exercise_session__workout" FOREIGN KEY ("workout_id") REFERENCES "workout" ("id");
+ALTER TABLE exercise_session ADD CONSTRAINT difficulty_check CHECK (difficulty >= 0 AND difficulty <= 10);
